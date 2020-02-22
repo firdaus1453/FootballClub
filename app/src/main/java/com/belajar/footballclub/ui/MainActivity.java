c7fbc9b517d2cf453eff034794b32a14295b5d65
firdaus1453 <mfirdaus1453@gmail.com>
2020-02-22 21:33:16 +0700

    Initial commit

---
+++ app/src/main/java/com/belajar/footballclub/ui/MainActivity.java
@@ -0,0 +1,15 @@
+package com.belajar.footballclub.ui;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.belajar.footballclub.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
